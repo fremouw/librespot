5f39eb27e0f50f96b900a8f12bfe7dd5ae41032b
Maarten Fremouw <maarten@fremouw.nl>
2020-04-26 12:44:19 +0200

    set volume of dummy mixer.

--- playback/src/mixer/dummymixer.rs
+++ playback/src/mixer/dummymixer.rs
@@ -6,21 +6,23 @@ use super::{Mixer, MixerConfig};
 
 #[derive(Clone)]
 pub struct DummyMixer {
-    // volume: Arc<AtomicUsize>,
+    volume: Arc<AtomicUsize>,
 }
 
 impl Mixer for DummyMixer {
     fn open(_: Option<MixerConfig>) -> DummyMixer {
         DummyMixer {
-            // volume: Arc::new(AtomicUsize::new(0xFFFF)),
+            volume: Arc::new(AtomicUsize::new(0xFFFF)),
         }
     }
     fn start(&self) {}
     fn stop(&self) {}
     fn volume(&self) -> u16 {
-        0
+        self.volume.load(Ordering::Relaxed) as u16
     }
-    fn set_volume(&self, volume: u16) {}
+    fn set_volume(&self, volume: u16) {
+        self.volume.store(volume as usize, Ordering::Relaxed);
+    }
     fn get_audio_filter(&self) -> Option<Box<dyn AudioFilter + Send>> {
         None
     }
